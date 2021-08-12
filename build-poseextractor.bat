0ae54ae7262d73c4b49f3d4e5a4e520670862ab5
Yasunori Shimura <yasu0710@gmail.com>
2021-08-12 17:43:17 +0900

    Fix build fail problem for Win10

--- build-poseextractor.bat
+++ build-poseextractor.bat
@@ -1,7 +1,9 @@
 xcopy /E "%INTEL_OPENVINO_DIR%\deployment_tools\open_model_zoo\demos\human_pose_estimation_3d_demo\python\pose_extractor" pose_extractor\
-mkdir build
+if not exist build (
+    mkdir build
+)
 cd build
 cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release -A x64 ..
-msbuild pose_extractor.sln /p:Configuration=Release
+cmake --build . --config release
 cd ..
 copy build\pose_extractor\Release\pose_extractor.pyd .
